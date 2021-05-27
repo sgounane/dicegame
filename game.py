06b5626a86820502af61eda4d9e32c757dc6ad00
gnu <gounane.said@gmail.com>
2021-05-27 10:41:00 +0100

    session

--- game.py
+++ game.py
@@ -1,26 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app=Flask(__name__)
 
-global logedin
-logedin=False
-
 @app.route("/")
 def index():
+    
     return render_template("index.html")
 
 
 @app.route("/game")
 def game():
-    global logedin
-    if logedin==True:
+    if session["logedin"]==True:
         return render_template("game.html")
     return render_template("login.html")
 @app.route("/login", methods=["POST","GET"])
 def login():
-    global logedin
-    if request.method=="POST" or logedin==True:
-        logedin=True
+    if request.method=="POST" or session["logedin"]==True:
+        session["logedin"]=True
         return  redirect("/game")
 
 
@@ -29,7 +25,7 @@ def login():
 
 @app.route("/logout")
 def logout():
-    global logedin
-    logedin=False
+    
+    session["logedin"]=False
     return render_template("logout.html")
 app.run(debug=True, host="0.0.0.0", port=4444)
