160c42394d571c92a95a7f8cbbcf507142c42ca5
gnu <gounane.said@gmail.com>
2021-05-27 10:45:56 +0100

    session

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, session
 
 app=Flask(__name__)
+app.config["SECRET_KEY"]="my secret key"
+
 
 @app.route("/")
 def index():
@@ -10,12 +12,12 @@ def index():
 
 @app.route("/game")
 def game():
-    if session["logedin"]==True:
+    if session.get("logedin")==True:
         return render_template("game.html")
     return render_template("login.html")
 @app.route("/login", methods=["POST","GET"])
 def login():
-    if request.method=="POST" or session["logedin"]==True:
+    if request.method=="POST" or session.get("logedin")==True:
         session["logedin"]=True
         return  redirect("/game")
 
